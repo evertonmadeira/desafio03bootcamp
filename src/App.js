b2f318fb868ecc9b1f6c93dac7d6896c24f7aead
Everton Madeira <everton.comp@gmail.com>
2020-04-10 18:53:55 -0300

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { useState, useEffect } from "react";
 import api from "./services/api";
 import "./styles.css";
-import AddRepo from "./components/AddRepo";
 
 function App() {
   const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [techs, setTechs] = useState("");
 
   useEffect(() => {
     api
       .get("repositories")
       .then((response) => setRepositories(response.data))
       .catch((error) => console.log(error));
-  }, [repositories]);
+  }, []);
+
+  async function handleAddRepository(e) {
+    e.preventDefault();
+
+    const repo = {
+      title,
+      url,
+      techs,
+    };
+
+    try {
+      const res = await api.post("repositories", repo);
+      setRepositories([...repositories, res.data]);
+    } catch (error) {
+      console.log(error);
+    }
+    setTitle();
+    setUrl();
+    setTechs();
+  }
 
   async function handleRemoveRepository(id) {
     try {
-      const res = await api.delete("repositories/" + id);
+      await api.delete(`repositories/${id}`);
+      setRepositories(
+        repositories.filter((repository) => repository.id !== id)
+      );
     } catch (error) {
       console.log(error);
     }
   }
 
   return (
-    <div className="container">
+    <>
       <div className="container-h1">
         <h1>Desafio 03 - Conceitos ReactJS</h1>
         <p>
@@ -30,21 +55,50 @@ function App() {
           API desenvolvida no Desafio 02.
         </p>
       </div>
-      <div>
-        <AddRepo />
-        <ul data-testid="repository-list">
-          <h1>Projetos</h1>
-          {repositories.map((repository) => (
-            <li key={repository.id}>
-              {repository.title}
-              <button onClick={() => handleRemoveRepository(repository.id)}>
-                Remover
-              </button>
-            </li>
-          ))}
-        </ul>
+      <div className="container">
+        <form>
+          <h1>Novo Projeto</h1>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="URL"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Techs, techs, techs"
+            value={techs}
+            onChange={(e) => setTechs(e.target.value)}
+          />
+          <button type="button" onClick={handleAddRepository}>
+            Adicionar
+          </button>
+        </form>
+
+        <div className="list-projects">
+          <h1 style={{ marginBottom: "5px" }}>Projetos</h1>
+          <ul data-testid="repository-list">
+            {repositories.map((repository) => (
+              <li key={repository.id}>
+                {repository.title}
+                <button
+                  // type="button"
+                  onClick={() => handleRemoveRepository(repository.id)}
+                >
+                  Remover
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
